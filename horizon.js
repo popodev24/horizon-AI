c1402bd6862e6ef0d8faf02490933167ab6e3960
popodev24 <mphombele399@gmail.com>
2026-05-24 20:31:04 +0200

    Add files via upload

---
+++ horizon.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener("click", () => {
+
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+
+});
+
